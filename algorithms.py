ecc026ed8c23ee71daa8734bb937fc6c0ea0030e
GalD101 <galdali10@gmail.com>
2022-09-29 11:13:51 +0300

    merge sort

--- algorithms.py
+++ algorithms.py
@@ -22,6 +22,33 @@ def lfsr():
         newbit = (state ^ (state >> 1) ^ (state >> 2) ^ (state >> 7))
         state = (state >> 1) | (newbit << 127)
 
-lfsr()
+
+
+def merge_sort(arr: list):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    return merge(left, right)
+
+def merge(l: list, r: list):
+    merged_list = []
+    while len(l) or len(r):
+        if len(l) and len(r):
+            if l[0] < r[0]:
+                merged_list.append(l.pop(0))
+            else:
+                merged_list.append(r.pop(0))
+        elif len(l):
+            merged_list.append(l.pop(0))
+        elif len(r):
+            merged_list.append(r.pop(0))
+    return merged_list
+
+
+
+print(merge_sort([2, 4, 1, 7, 5, 3, 6, 8]))
+# lfsr()
 # print(square_and_multiply(23, 373, 747))
 # print(square_and_multiply(3, 45, 7))
